44f3f23fd9e22e8cd7de13a345b24c2af2fd7c77
ajaymood <ajaymood@buffalo.edu>
2024-08-13 00:28:25 -0400

    add languages

--- main/hello.go
+++ main/hello.go
@@ -4,10 +4,30 @@ import (
 	"fmt"
 )
 
-func Hello(name string) string {
-	return "Hello " + name
+const (
+	enHelloPrefix = "Hello "
+	esHelloPrefix = "Hola "
+	frHelloPrefix = "Bonjour "
+)
+
+func Hello(name, lang string) string {
+	if name == "" {
+		if lang == "es" {
+			name = "mundo"
+		} else if lang == "fr" {
+			name = "monde"
+		} else {
+			name = "world"
+		}
+	}
+	if lang == "es" {
+		return esHelloPrefix + name
+	} else if lang == "fr" {
+		return frHelloPrefix + name
+	}
+	return enHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "en"))
 }
--- main/hello_test.go
+++ main/hello_test.go
@@ -3,9 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "en")
+		want := "Hello Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("emtpy string defaults to world", func(t *testing.T) {
+		got := Hello("", "en")
+		want := "Hello world"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("hello world in Spanish", func(t *testing.T) {
+		got := Hello("", "es")
+		want := "Hola mundo"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("hello Chris in spanish", func(t *testing.T) {
+		got := Hello("Chris", "es")
+		want := "Hola Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("hello Chris in french", func(t *testing.T) {
+		got := Hello("Chris", "fr")
+		want := "Bonjour Chris"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
